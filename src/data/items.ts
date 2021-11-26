f48e075e1045a849c28a510c18967405661b2467
FabricioCaldana <fanelli@grupodimed.com.br>
2021-11-26 14:47:43 -0300

    Projeto na Metade

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2021, 10, 26), category: 'food', title: 'McDonalds', value: 20 },
+    { date: new Date(2021, 10, 20), category: 'food', title: 'Burguer King', value: 32 },
+    { date: new Date(2021, 10, 23), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2021, 11, 26), category: 'salary', title: 'Salário Dimed', value: 1079 },
+];
